856887c5579525828f9bdc7067adbbe8b6508720
Kalaigar-Ayesha <ayesha389922@gmail.com>
2025-05-29 22:23:04 +0530

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/Job-Application-Tracker/' ,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
